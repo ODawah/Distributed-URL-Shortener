f7223127ba9459c8f26ad2b44315c121d9747981
ODawah <omardawah550@gmail.com>
2025-02-14 22:40:09 +0200

    added the models

--- models/requests.go
+++ models/requests.go
@@ -1,1 +1,9 @@
 package models
+
+import "time"
+
+type RequestData struct {
+	ShortID   string    `json:"shortID"`
+	Timestamp time.Time `json:"timestamp"`
+	IP        string    `json:"IP"`
+}
--- models/url.go
+++ models/url.go
@@ -1,6 +1,20 @@
 package models
 
+import (
+	"github.com/jaevor/go-nanoid"
+)
+
 type URL struct {
-	ID  string `json:"id" binding:"required"`
-	URL string `json:"url"`
+	URL string `json:"url" binding:"required"`
+	ID  string `json:"id"`
+}
+
+func (Url *URL) Shorten() error {
+	canonicID, err := nanoid.Standard(8)
+	if err != nil {
+		return err
+	}
+	id := canonicID()
+	Url.ID = id
+	return nil
 }
